4f6df0fb0d3e9787a19c4e268704a68724062eed
eben-siaw <61696999+eben-siaw@users.noreply.github.com>
2020-10-22 12:49:58 +0100

    new fixes

--- src/Pages/InstructorPage/VideoConference/VideoConfig.js
+++ src/Pages/InstructorPage/VideoConference/VideoConfig.js
@@ -525,7 +525,7 @@ class VideoConfig extends Component {
           zIndex: 3,
           position: 'absolute',
         }}>
-          <i onClick={(e) => {this.setState({disconnected: true})}} style={{ cursor: 'pointer', paddingLeft: 15, color: 'red' }} className='ion-closed-circled'></i>
+          <i onClick={(e) => {this.setState({disconnected: true})}} style={{ cursor: 'pointer', paddingLeft: 15, color: 'red' }} className='ion-close-circled'></i>
           <div  
           className="status"
        >{ statusText }</div>
